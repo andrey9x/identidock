0fdefc99d50754ca1c47833800053d438284994d
Andrey <prog.qt@gmail.com>
2019-08-12 23:41:51 +0300

    add post update and hash img

--- app/identidock.py
+++ app/identidock.py
@@ -1,23 +1,28 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import hashlib
 
 app = Flask(__name__)
+salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
 
     name = default_name
+    if request.method == 'POST':
+        name = request.form['name']
+    salted_name = salt + name
+    name_hash = hashlib.sha256( salted_name.encode() ).hexdigest()
     
-    header = '<html><head><title>Identidock</title></head><body>'
-    
+    header = '<html><head><title>Identidock</title></head><body>'    
     body = '''<form method="POST">
-            Hello <input type="text" name="name" value="{}">
+            Hello <input type="text" name="name" value="{0}">
             <input type="submit" value="submit">
             </form>
             <p>You look like a:
-            <img src="/monster/monster.png?size=80"/>
-            '''.format(name)
+            <img src="/monster/{1}"/>
+            '''.format(name, name_hash)
     footer = '</body></html>'
     
     return header + body + footer
